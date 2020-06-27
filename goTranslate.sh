47da592115742108eea316bd3eb6932231d20462
yuanlehome <yuanlehome@163.com>
2020-06-27 17:42:04 +0800

    Update goTranslate.sh

--- goTranslate.sh
+++ goTranslate.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-str_old=$(cat ~/Translator/lastContent.txt)
+str_old=$(cat ~/Translator/lastContent)
 str_new=$(xclip -o 2>/dev/null | xargs)
 if [[ "$str_new" != "$str_old" && $str_new ]]; then
 	echo -e "\n"
-	echo "$str_new" | xargs | trans :zh-CN -b
+	count=$(echo "$str_new" | wc -w)
+	if [ "$count" == "1" ]; then
+        	echo -n -e "$str_new " >> ~/Translator/words
+		echo "$str_new" | trans :zh-CN | tail -1 | cut -c 5- | sed "s,\x1b\[[0-9;]*[a-zA-Z],,g" | tee -a ~/Translator/words
+	else
+		echo "$str_new" | trans :zh-CN -b
+	fi
+    	echo "$str_new" > ~/Translator/lastContent
 fi
-echo "$str_new" | xargs > ~/Translator/lastContent.txt
